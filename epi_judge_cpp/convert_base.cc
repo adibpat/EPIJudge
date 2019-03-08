27b11db92ecac19831679ed335488a0c82a6c7e0
Aditya Patwardhan <abpatwar@ncsu.edu>
2019-03-07 23:37:44 -0500

    convert_base & primitive_multiply

--- epi_judge_cpp/convert_base.cc
+++ epi_judge_cpp/convert_base.cc
@@ -3,8 +3,32 @@
 using std::string;
 
 string ConvertBase(const string& num_as_string, int b1, int b2) {
-  // TODO - you fill in here.
-  return "";
+  unsigned long long x = 0;
+
+  for (size_t i = (num_as_string.front() == '-' ? 1:0); i < num_as_string.size(); i++) {
+    x = x*b1;
+    if (std::isdigit(num_as_string[i])) {
+      x += num_as_string[i] - '0';
+    } else {
+      x += num_as_string[i] - 'A' + 10;
+    }
+  }
+  if (x == 0) {
+    return num_as_string.front() == '-'? "-0" : "0";
+  }
+  string result;
+  while (x) {
+    if (x%b2 < 10) { 
+      result.push_back(x%b2 + '0');
+    } else {
+      result.push_back(x%b2 - 10 + 'A');
+    }
+    x /= b2;
+  };
+
+  if (num_as_string.front() == '-') result.push_back('-');
+  std::reverse(result.begin(), result.end());
+  return result;
 }
 
 int main(int argc, char* argv[]) {
--- epi_judge_cpp/primitive_multiply.cc
+++ epi_judge_cpp/primitive_multiply.cc
@@ -1,7 +1,34 @@
 #include "test_framework/generic_test.h"
+
+unsigned long long AddWoUsingAdd(unsigned long long val1, unsigned long long val2) {
+  //std::cout << "Adding " << val1 << " and " << val2 << std::endl;
+  unsigned long long result = 0, carry = 0;
+  int i = 0;
+  while(val1 || val2) {
+    result = result | (((val1 & 1) ^ (val2 & 1) ^ carry) << i);
+    if ((val1 & 1) ^ (val2 & 1)) {
+      carry = (carry & 1);
+    } else {
+      carry = (val1 & 1);
+    }
+    val1 = val1 >> 1; val2 = val2 >> 1;
+    i++;
+  }
+  //std::cout << "Result is " << (result | (carry << i)) << std::endl;
+  return (result | (carry << i));
+}
+
 unsigned long long Multiply(unsigned long long x, unsigned long long y) {
-  // TODO - you fill in here.
-  return 0;
+  int i = 0; 
+  unsigned long long result = 0;
+  while (y) {
+    if ((1L << i) & y) {
+      result = AddWoUsingAdd(result, x << i);
+      y = y & (y-1);
+    }
+    i++;
+  }
+  return (result);
 }
 
 int main(int argc, char* argv[]) {
