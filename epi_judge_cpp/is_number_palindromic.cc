862a931d27b154ed3ebf1abd3048fa1699213392
Aditya Patwardhan <abpatwar@ncsu.edu>
2019-03-09 10:24:38 -0500

    is_number_palindromic.cc

--- epi_judge_cpp/is_number_palindromic.cc
+++ epi_judge_cpp/is_number_palindromic.cc
@@ -1,5 +1,6 @@
 #include "test_framework/generic_test.h"
 bool IsPalindromeNumber(int x) {
+  /* O(n) solution
   if (x < 0) return (false);
   int comp = 0; int temp = x;
   while (temp) {
@@ -7,6 +8,20 @@ bool IsPalindromeNumber(int x) {
     temp /= 10;
   }
   return (x == comp);
+  */
+
+  /* Optimized O(n) solution */
+  if (x < 0) return (false);
+  int num_of_digits = static_cast<int> (floor(log10(x))) + 1; // log10(1) = 0, log10(10) = 1, log10(100) = 2,..so # of digits is log10(x) + 1
+  int most_sig_power = static_cast<int> (pow(10, num_of_digits -1));
+  int remaining_digits = x;
+  for (int i = 0; i < num_of_digits/2; ++i) {
+    if (x/most_sig_power != remaining_digits%10) return (false);
+    remaining_digits /= 10;
+    x %= most_sig_power;
+    most_sig_power /= 10;
+  }
+  return (true);
 }
 
 int main(int argc, char* argv[]) {
